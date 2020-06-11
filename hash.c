3cfc7a12fd8c4acf13e0e4862eb56aaa7b1106d9
e <15608652022@163.com>
2020-06-12 00:33:36 +0800

    error

--- hash.c
+++ hash.c
@@ -116,12 +116,17 @@ int insert_hash(HASH *dir, Key_t new_key, Value_t new_value)
     else
     {
         printf("split 2 begin...\n");
+        for (i = 0; i < (1 << dir->global_depth); ++i) {
+			printf("%p %x\n", dir->_->_[i],dir->_->_[i]->pattern);
+		}
         Segment *new_Segment = getNode(HASH_SEGMENT);
         memset(new_Segment, -1, sizeof(struct Segment));
         new_Segment->pattern = (1 << dir_->local_depth) + dir_->pattern;
         new_Segment->local_depth = dir_->local_depth + 1;
         printf("new_Segment=%p, local_depth=%d, pattern=%d\n", new_Segment,new_Segment->local_depth,new_Segment->pattern);
-
+        for (i = 0; i < (1 << dir->global_depth); ++i) {
+			printf("%p %x\n", dir->_->_[i],dir->_->_[i]->pattern);
+		}
         for (i = 0; i < kNumSlot; ++i)
         {
             if (dir_->_[i].key != INVALID)
@@ -148,9 +153,9 @@ int insert_hash(HASH *dir, Key_t new_key, Value_t new_value)
         }
         pmem_persist(new_Segment, sizeof(new_Segment));
         mfence();
-        for (i = 0; i < (1 << dir->global_depth); ++i) {
-			printf("%p %x\n", dir->_->_[i],dir->_->_[i]->pattern);
-		}
+        // for (i = 0; i < (1 << dir->global_depth); ++i) {
+		// 	printf("%p %x\n", dir->_->_[i],dir->_->_[i]->pattern);
+		// }
 
         unsigned int tail = new_Segment->pattern;
         while (tail < (1 << dir->global_depth))

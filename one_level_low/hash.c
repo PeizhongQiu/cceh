1c2ccdb49300aab2b2985aeb7f2c3ea3cb6bb435
e <15608652022@163.com>
2021-03-15 15:31:36 +0800

    one_level_low

---
+++ one_level_low/hash.c
@@ -0,0 +1,279 @@
+#include "hash.h"
+#include "memory_management.h"
+
+#ifdef DEBUG_TIME
+    u64 resize_time = 0;
+    u64 resize_1_num = 0;
+    u64 resize_2_num = 0;
+    u64 malloc_time = 0;
+    void print_resize()
+    {
+        printf("resize_time = %lld\n", resize_time);
+        printf("resize_1_num = %lld\n", resize_1_num);
+        printf("resize_2_num = %lld\n", resize_2_num);
+    }
+#endif
+
+size_t unaligned_load(const char *p)
+{
+  size_t result;
+  __builtin_memcpy(&result, p, sizeof(result));
+  return result;
+}
+
+size_t load_bytes(const char *p, int n)
+{
+  size_t result = 0;
+  --n;
+  do
+    result = (result << 8) + (unsigned char)(p[n]);
+  while (--n >= 0);
+  return result;
+}
+
+size_t shift_mix(size_t v)
+{
+  return v ^ (v >> 47);
+}
+
+size_t _Hash_bytes(const void *ptr, size_t len, size_t seed)
+{
+  static const size_t mul = (((size_t)0xc6a4a793UL) << 32UL) + (size_t)0x5bd1e995UL;
+  const char *const buf = (const char *)(ptr);
+
+  // Remove the bytes not divisible by the sizeof(size_t).  This
+  // allows the main loop to process the data as 64-bit integers.
+  const size_t len_aligned = len & ~(size_t)0x7;
+  const char *const end = buf + len_aligned;
+  size_t hash = seed ^ (len * mul);
+  const char *p = buf;
+  for (; p != end; p += 8)
+  {
+    const size_t data = shift_mix(unaligned_load(p) * mul) * mul;
+    hash ^= data;
+    hash *= mul;
+  }
+  if ((len & 0x7) != 0)
+  {
+    const size_t data = load_bytes(end, len & 0x7);
+    hash ^= data;
+    hash *= mul;
+  }
+  hash = shift_mix(hash) * mul;
+  hash = shift_mix(hash);
+  return hash;
+}
+
+size_t hash_64(size_t val)
+{
+    return _Hash_bytes(&val, sizeof(size_t), 0xc70f6907UL);
+}
+
+void print(HASH *dir)
+{
+    printf("%d\n", dir->global_depth);
+    int i, j;
+    for (i = 0; i < (1 << dir->global_depth); ++i)
+    {
+        printf("(%016llx, %016p)", dir->_->_[i]->pattern, dir->_->_[i]);
+    }
+    printf("\n");
+    for (j = 0; j < kNumSlot; ++j)
+    {
+        for (i = 0; i < (1 << dir->global_depth); ++i)
+        {
+            Segment *seg = dir->_->_[i];
+
+            printf("(%016llx, %016llx)", seg->_[j].key, hash_64(seg->_[j].key));
+        }
+        printf("\n");
+    }
+}
+
+Segment * Segment_Split(Segment *seg)
+{
+    Segment *new_Segment = getNode(HASH_SEGMENT, 0);
+    memset(new_Segment, -1, sizeof(struct Segment));
+
+    new_Segment->pattern = (seg->pattern) + (1 << seg->local_depth);
+    new_Segment->local_depth = seg->local_depth + 1;
+    #ifdef DEBUG_ERROR
+        printf("new_Segment=%p, local_depth=%d, pattern=%d\n", new_Segment,new_Segment->local_depth,new_Segment->pattern);
+    #endif
+    int i;
+    for (i = 0; i < kNumSlot; ++i)
+    {
+        if (seg->_[i].key != INVALID)
+        {
+            Key_t re_hash_key = hash_64(seg->_[i].key);
+            size_t pattern = re_hash_key & ((1 << new_Segment->local_depth) - 1);
+            if (pattern == new_Segment->pattern)
+            {
+                new_Segment->_[i].value = seg->_[i].value;
+                new_Segment->_[i].key = seg->_[i].key;     
+            }
+        }
+    }
+    pmem_persist(new_Segment, sizeof(Segment));
+    mfence();
+    return new_Segment;
+}
+
+int insert_hash(HASH *dir, Key_t new_key, Value_t new_value)
+{
+    Key_t key_hash = hash_64(new_key);
+
+    Key_t x = key_hash & ((1 << dir->global_depth) - 1);
+
+    Segment *seg = dir->_->_[x];
+    unsigned i;
+    for (i = 0; i < kNumSlot; ++i)
+    {
+        if (seg->_[i].key == INVALID ||
+            (hash_64(seg->_[i].key) & ((1 << seg->local_depth) - 1)) != seg->pattern)
+        {
+            seg->_[i].value = new_value;
+            seg->_[i].key = new_key;
+            pmem_persist(&seg->_[i], sizeof(Pair));
+            mfence();
+            //print(dir);
+            return 1;
+        }
+    }
+
+    if (seg->local_depth == dir->global_depth)
+    {
+        //分配一个新的Segment
+        #ifdef DEBUG_TIME
+            ++resize_1_num;
+            struct timeval start, end;
+            mfence();
+            gettimeofday(&start, NULL);
+            mfence();
+        #endif
+        #ifdef DEBUG_ERROR
+            printf("split 1 begin...\n");
+        #endif
+        Segment *new_Segment = Segment_Split(seg);
+        //Directory翻倍
+        Directory *new_dir = getNode(HASH_DIRECTORY, 1<<new_Segment->local_depth);
+        for (i = 0; i < (1 << (dir->global_depth)); ++i)
+        {
+            new_dir->_[i] = dir->_->_[i];
+            if (i == x)
+            {
+                new_dir->_[i + (1 << dir->global_depth)] = new_Segment;
+            }
+            else
+            {
+                new_dir->_[i + (1 << dir->global_depth)] = dir->_->_[i];
+            }
+        }
+        dir->_ = new_dir;
+        pmem_persist(dir->_, sizeof(Directory));
+        mfence();
+
+        ++seg->local_depth;
+        pmem_persist(&seg->local_depth, sizeof(size_t));
+        mfence();
+        ++dir->global_depth;
+        pmem_persist(dir, sizeof(HASH));
+        mfence();
+        #ifdef DEBUG_TIME
+            mfence();
+            gettimeofday(&end, NULL);
+            mfence();
+            resize_time += (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+        #endif
+    }
+    else
+    {
+        #ifdef DEBUG_TIME
+            resize_2_num++;
+            struct timeval start, end;
+            mfence();
+            gettimeofday(&start, NULL);
+            mfence();
+        #endif
+        #ifdef DEBUG_ERROR
+            printf("split 2 begin...\n");
+        #endif
+        Segment *new_Segment = Segment_Split(seg);
+
+        for (i = new_Segment->pattern; i < (1 << (dir->global_depth)); i = i+(1<<new_Segment->local_depth))
+        {
+            dir->_->_[i] = new_Segment;
+        }
+
+        pmem_persist(dir->_, sizeof(Directory));
+        mfence();
+        ++seg->local_depth;
+        pmem_persist(&seg->local_depth, sizeof(size_t));
+        mfence();
+        //printf("new_Segment %p\n", new_Segment);
+        /*	for (i = 0; i < (1 << dir->global_depth); ++i) {
+			printf("%p %x\n", dir->_->_[i],dir->_->_[i]->pattern);
+		}*/
+        #ifdef DEBUG_TIME
+            mfence();
+            gettimeofday(&end, NULL);
+            mfence();
+            resize_time += (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+        #endif
+    }
+    return insert_hash(dir, new_key, new_value);
+}
+
+int delete_hash(HASH *dir, Key_t search_key)
+{
+    Key_t key_hash = hash_64(search_key);
+    Key_t x = key_hash & ((1 << dir->global_depth) - 1);
+    Segment *seg = dir->_->_[x];
+    unsigned i;
+    for (i = 0; i < kNumSlot; ++i)
+    {
+        if (seg->_[i].key == search_key)
+        {
+            seg->_[i].key = INVALID;
+            pmem_persist(&seg->_[i].key, sizeof(Key_t));
+            mfence();
+            //            free(seg->_[slot].value);
+            return 1;
+        }
+    }
+    return NONE;
+}
+
+Value_t search_hash(HASH *dir, Key_t search_key)
+{
+    Key_t key_hash = hash_64(search_key);
+    Key_t x = key_hash & ((1 << dir->global_depth) - 1);
+
+    Segment *seg = dir->_->_[x];
+    unsigned i;
+    for (i = 0; i < kNumSlot; ++i)
+    {
+        if (seg->_[i].key == search_key)
+        {
+            return seg->_[i].value;
+        }
+    }
+    return NONE;
+}
+
+void init(HASH *o_hash)
+{
+    o_hash->global_depth = 1;
+    o_hash->_ = getNode(HASH_DIRECTORY, 2);
+    Directory *init_dir = o_hash->_;
+    
+    init_dir->_[0] = getNode(HASH_SEGMENT, 0);
+    memset(init_dir->_[0], -1, sizeof(Segment));
+    init_dir->_[0]->local_depth = 1;
+    init_dir->_[0]->pattern = 0;
+
+    init_dir->_[1] = getNode(HASH_SEGMENT, 0);
+    memset(init_dir->_[1], -1, sizeof(Segment));
+    init_dir->_[1]->local_depth = 1;
+    init_dir->_[1]->pattern = 1;
+}
---
+++ one_level_low/memory_management.c
@@ -0,0 +1,101 @@
+#include "memory_management.h"
+#include <libpmem.h>
+#include "hash.h"
+
+//注意第二维度从1开始
+//存放每个块的地址
+void *table[HASH_TYPE][10000];
+//NvmBlock * table[100000000/MEMORY_BLOCK_LEN +1];
+//MemoryBlock * mem_table[100000000/MEMORY_BLOCK_LEN +1];
+
+//每个nvmblock对应一个4M块
+//NVMBLOCK_DECLARE(Directory);
+NVMBLOCK_DECLARE(Segment);
+
+//mm是每种类型的table，已经分配的数量
+int mm[HASH_TYPE] = {0};
+
+void *add_pmalloc(int type, size_t size, size_t *mapped_len)
+{
+	char path[100];
+	switch (type)
+	{
+	case HASH_DIRECTORY:
+		mm[HASH_DIRECTORY]++;
+		sprintf(path, "/mnt/dax_200g/DIRECTORY_%d", mm[HASH_DIRECTORY]);
+		break;
+	case HASH_SEGMENT:
+		mm[HASH_SEGMENT]++;
+		sprintf(path, "/mnt/dax_200g/SEGMENT_%d", mm[HASH_SEGMENT]);
+		break;
+	// case ARRAY:
+	// 	mm[ARRAY] = __sync_fetch_and_add(&mm[ARRAY], 1);
+	// 	sprintf(path, "/mnt/dax/ARRAY_%d", mm[ARRAY]);
+	// 	break;
+	default:
+		printf("add_pmalloc: wrong type!\n");
+		return NULL;
+	}
+
+	void *addr;
+	int is_pmem;
+
+	if ((addr = pmem_map_file(path, size, PMEM_FILE_CREATE,
+							  0666, mapped_len, &is_pmem)) == NULL)
+	{
+		return NULL;
+	}
+
+	return addr;
+}
+
+void *getNode(int type, int size)
+{
+	switch (type)
+	{
+	case HASH_DIRECTORY:;
+		size_t mapped_len;
+		Directory *dir = malloc(sizeof(Directory));
+		dir->_ = (Segment**)add_pmalloc(type, size*sizeof(Segment*), &mapped_len);
+		if (!dir->_)
+		{
+			printf("newBlock creation fails: nvm\n");
+			exit(1);
+		}
+		return dir;
+	case HASH_SEGMENT:;
+		NVMBLOCK(Segment) *nvm_Block_Segment = table[type][mm[type]];
+		
+		if (nvm_Block_Segment == NULL || nvm_Block_Segment->used_num == MEMORY_BLOCK_LEN(Segment))
+		{
+			nvm_Block_Segment = getNvmBlock(type);
+			nvm_Block_Segment->used_num = 0;
+		}
+		//printf("%u %u\n",nvm_Block_Segment->used_num,MEMORY_BLOCK_LEN(Segment));
+		//nvm_Block->used_num++;
+		return &nvm_Block_Segment->data[nvm_Block_Segment->used_num++];
+	default:
+		printf("add_pmalloc: wrong type!\n");
+		return NULL;
+	}
+}
+
+void *getNvmBlock(int type)
+{
+	size_t mapped_len;
+	void *newBlock = add_pmalloc(type, BLOCK_SIZE, &mapped_len);
+	if (!newBlock)
+	{
+		printf("newBlock creation fails: nvm\n");
+		exit(1);
+	}
+
+	table[type][mm[type]] = newBlock;
+	return newBlock;
+}
+
+void mfence()
+{
+    asm volatile("mfence" ::
+                     : "memory");
+}
